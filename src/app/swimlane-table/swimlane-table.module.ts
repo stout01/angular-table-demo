be7429586503a3cfe5bb1b1ec748b84bcac7a93d
Andrew Stout <andrew@andrewstout.me>
2019-01-20 17:09:11 -0600

    Attempted to setup swimlane table

--- src/app/swimlane-table/swimlane-table.module.ts
+++ src/app/swimlane-table/swimlane-table.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 import { SwimlaneTableRoutingModule } from './swimlane-table-routing.module';
 import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [TableComponent],
-  imports: [CommonModule, SwimlaneTableRoutingModule],
+  imports: [CommonModule, SwimlaneTableRoutingModule, NgxDatatableModule],
 })
 export class SwimlaneTableModule {}
--- src/app/swimlane-table/table/table.component.ts
+++ src/app/swimlane-table/table/table.component.ts
@@ -1,12 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { StockQuote } from '../../core/models/stock-quote';
+import { StockMarketService } from '../../core/services/stock-market.service';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class TableComponent implements OnInit {
-  constructor() {}
+  public stocks: Observable<StockQuote[]>;
 
-  public ngOnInit() {}
+  public columns = [
+    { name: 'CompanyName', prop: 'companyName' },
+    { name: 'Symbol', prop: 'symbol' },
+    { name: 'Latest Price', prop: 'latestPrice' },
+    { name: 'Change', prop: 'change' },
+    { name: 'Exchange', prop: 'primaryExchange' },
+  ];
+
+  constructor(private readonly stockMarketService: StockMarketService) {}
+
+  public ngOnInit() {
+    this.stocks = this.stockMarketService.getHealthCareStocks$();
+  }
 }
