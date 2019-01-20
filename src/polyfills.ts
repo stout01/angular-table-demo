24bb10f494f2eb5db02736e0a83e21cfd126bff1
Andrew Stout <andrew@andrewstout.me>
2019-01-20 17:36:12 -0600

    Added es7 reflect to polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -42,6 +42,7 @@
 
 /** IE10 and IE11 requires the following for the Reflect API. */
 // import 'core-js/es6/reflect';
+import 'core-js/es7/reflect';
 
 /**
  * Web Animations `@angular/platform-browser/animations`
