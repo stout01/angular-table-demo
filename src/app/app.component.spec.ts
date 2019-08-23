a59f114ae5545eca21d18f799460907a0b7a53cb
Andrew Stout <andrew@andrewstout.me>
2019-08-22 19:23:21 -0500

    Fixed unit tests and removed empty unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -27,6 +27,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-table-demo!');
+    expect(compiled.querySelector('h1').textContent).toContain('Angular Table Demo');
   });
 });
--- src/app/core/services/stock-market.service.spec.ts
+++ src/app/core/services/stock-market.service.spec.ts
@@ -1,9 +1,10 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { StockMarketService } from './stock-market.service';
 
 describe('StockMarketService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({ imports: [HttpClientTestingModule] }));
 
   it('should be created', () => {
     const service: StockMarketService = TestBed.get(StockMarketService);
